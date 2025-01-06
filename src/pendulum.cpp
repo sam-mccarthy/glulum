a5aa61a88fc5e5bc3cd2f9af1b550a6aa15412eb
sam-mccarthy <samuelmccarthy03@gmail.com>
2025-01-06 01:42:29 -0500

    Began implementation of double pendulum

--- src/pendulum.cpp
+++ src/pendulum.cpp
@@ -1,15 +1,51 @@
 #include "pendulum.h"
 
-Pendulum::Pendulum(int w, int h){
+Pendulum::Pendulum(int w, int h, Parameters p){
     _width = w;
     _height = h;
 
+    _params = p;
+
+    _theta_1 = new double[_width * _height];
+    _theta_2 = new double[_width * _height];
+
+    _d_theta_1 = new double[_width * _height];
+    _d_theta_2 = new double[_width * _height];
+
+    _dd_theta_1 = new double[_width * _height];
+    _dd_theta_2 = new double[_width * _height];
 }
 
 Pendulum::~Pendulum(){
+    delete _theta_1;
+    delete _theta_2;
 
+    delete _d_theta_1;
+    delete _d_theta_2;
+
+    delete _dd_theta_1;
+    delete _dd_theta_2;
+}
+
+std::pair<double, double> Pendulum::calculate_dd(double t1, double t2, double v1, double v2){
+    double g = _params.gravity;
+    double m1 = _params.mass_1;
+    double m2 = _params.mass_2;
+    double l1 = _params.length_1;
+    double l2 = _params.length_2;
+
+    //double w1 = -g * (2 * m1 + m2) - m2 * g * sin(t1 - 2 * t2) - 2 * sin(t1 - t2) * m2 * ();
+}
+
+std::pair<double, double> Pendulum::calculate_d(double t1, double t2, double v1, double v2){
+    double h = _params.step_size;
+    
 }
 
 std::unique_ptr<unsigned char> Pendulum::step(double delta_t){
-
+    for(int x = 0; x < _width; x++){
+        for(int y = 0; y < _height; y++){
+            _dd_theta_1[x + y * _width] = 
+        }
+    }
 }
--- src/pendulum.h
+++ src/pendulum.h
@@ -2,15 +2,30 @@
 
 #include <memory>
 
+struct Parameters {
+    double gravity;
+
+    double mass_1;
+    double mass_2;
+
+    double length_1;
+    double length_2;
+
+    double step_size;
+};
+
 class Pendulum {
 public:
-    Pendulum(int w, int h);
+    Pendulum(int w, int h, Parameters p);
     ~Pendulum();
+    std::pair<double, double> calculate_dd(double t1, double t2, double v1, double v2);
+    std::pair<double, double> calculate_d(double t1, double t2, double v1, double v2);
     std::unique_ptr<unsigned char> step(double delta_t);
-    
 private:
     int _width;
     int _height;
+
+    Parameters _params;
 
     double* _theta_1;
     double* _d_theta_1;
